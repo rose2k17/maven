5ff66469bafb1fd0eab5c44130c586d5758b8253
Curso mañana <Curso maana@PC17a5>
2018-01-29 14:36:07 +0100

    Hay que meter bootstrap en casa y que se sincronice aqui en eclipse.

---
+++ springapp/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h1>Example - Spring Application</h1>
+<p>This is my test.</p>
+<a href="hello.htm">Ir a Hello</a>
+<a href="servicios.htm">Ir a servicios</a>
+</body>
+</html>
---
+++ springapp/src/test/java/com/curso/springapp/web/HelloControllerTests.java
@@ -0,0 +1,19 @@
+package com.curso.springapp.web;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+
+public class HelloControllerTests {
+
+    @Test
+    public void testHandleRequestView() throws Exception{		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("hello.jsp", modelAndView.getViewName());
+    }
+
+}
